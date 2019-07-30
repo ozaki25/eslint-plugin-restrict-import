d374d25767092f85e13ff6ada0b3c333ff0a2943
ozaki <fs65ugumori@yahoo.co.jp>
2019-07-31 08:08:13 +0900

    create sample rule

--- eslint/rule/no-import-react-native-text.js
+++ eslint/rule/no-import-react-native-text.js
@@ -5,6 +5,21 @@ module.exports = {
     type: 'suggestion',
   },
   create(context) {
-    return {};
+    return {
+      ImportDeclaration(node) {
+        const moduleName = node.source.value;
+        if (moduleName === 'react-native') {
+          const names = node.specifiers.map(specifier => {
+            return specifier.type === 'ImportSpecifier'
+              ? specifier.imported.name
+              : null;
+          });
+          if (names.includes('Text')) {
+            const message = 'This import is not allowed.';
+            context.report({ node, message });
+          }
+        }
+      },
+    };
   },
 };
--- eslint/test/no-import-react-native-text.js
+++ eslint/test/no-import-react-native-text.js
@@ -3,11 +3,40 @@
 const RuleTester = require('eslint').RuleTester;
 const tester = new RuleTester();
 
+const parserOptions = {
+  sourceType: 'module',
+  ecmaVersion: 6,
+};
+
 tester.run(
   'no-import-react-native-text',
   require('../rule/no-import-react-native-text'),
   {
-    valid: [],
-    invalid: [],
+    valid: [
+      {
+        code: "import { View } from 'react-native';",
+        parserOptions,
+      },
+      {
+        code: "import { Text } from 'foo';",
+        parserOptions,
+      },
+      {
+        code: "import Text from 'react-native';",
+        parserOptions,
+      },
+    ],
+    invalid: [
+      {
+        code: "import { Text } from 'react-native';",
+        parserOptions,
+        errors: ['This import is not allowed.'],
+      },
+      {
+        code: "import { Text, View } from 'react-native';",
+        parserOptions,
+        errors: ['This import is not allowed.'],
+      },
+    ],
   },
 );
