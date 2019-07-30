39b217456a1ee0348d8f320de988c408461e4635
ozaki <fs65ugumori@yahoo.co.jp>
2019-07-31 08:11:22 +0900

    add example

---
+++ example/src/valid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TextInput, View } from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+}
+
+export default App;
