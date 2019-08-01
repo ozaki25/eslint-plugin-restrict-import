6f690d04f7af5d4419ee563ebced35835d731061
ozaki <fs65ugumori@yahoo.co.jp>
2019-08-01 23:54:50 +0900

    add no-specific-import rule

---
+++ tests/lib/rules/no-specific-import.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const RuleTester = require('eslint').RuleTester;
+const tester = new RuleTester({
+  parserOptions: { sourceType: 'module', ecmaVersion: 2015 },
+});
+
+tester.run(
+  'no-specific-import',
+  require('../../../lib/rules/no-specific-import'),
+  {
+    valid: [
+      {
+        code: "import { baz } from 'foo';",
+        options: [[['foo', 'bar']]],
+      },
+      {
+        code: "import { bar } from 'baz';",
+        options: [[['foo', 'bar']]],
+      },
+      {
+        code: "import bar from 'foo';",
+        options: [[['foo', 'bar']]],
+      },
+    ],
+    invalid: [
+      {
+        code: "import { bar } from 'foo';",
+        options: [[['foo', 'bar']]],
+        errors: ['Import of foo bar is not allowed.'],
+      },
+      {
+        code: "import { bar, baz } from 'foo';",
+        options: [[['foo', 'bar']]],
+        errors: ['Import of foo bar is not allowed.'],
+      },
+      {
+        code: "import { fiz } from 'baz';",
+        options: [[['foo', 'bar'], ['baz', 'fiz']]],
+        errors: ['Import of baz fiz is not allowed.'],
+      },
+      {
+        code: "import { bar } from 'foo';import { fiz } from 'baz';",
+        options: [[['foo', 'bar'], ['baz', 'fiz']]],
+        errors: [
+          'Import of foo bar is not allowed.',
+          'Import of baz fiz is not allowed.',
+        ],
+      },
+    ],
+  },
+);
